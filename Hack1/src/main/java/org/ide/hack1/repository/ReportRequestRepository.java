ca76b697405311b666df572088c535b03cb304ca
Rafael Choque <rafael.choquel@utec.edu.pe>
2025-10-21 18:47:02 -0500

    termine de terminar

--- Hack1/src/main/java/org/ide/hack1/repository/ReportRequestRepository.java
+++ Hack1/src/main/java/org/ide/hack1/repository/ReportRequestRepository.java
@@ -1,4 +1,9 @@
 package org.ide.hack1.repository;
+import org.ide.hack1.entity.ReportRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public class ReportRequestRepository {
+public interface ReportRequestRepository extends JpaRepository<ReportRequest, String> {
+    List<ReportRequest> findByRequestedBy(String requestedBy);
+    List<ReportRequest> findByStatus(org.ide.hack1.entity.ReportRequest.Status status);
 }
